a27d4fe67eb0b264dce1ceed272b6d76ec913c1d
Steve Albers <stevealbers@hotmail.com>
2018-07-11 13:28:44 -0400

    Added in PWA install event code

--- js/app.js
+++ js/app.js
@@ -7,3 +7,33 @@ if ('serviceWorker' in navigator) {
         console.log(err);
       });
 }
+
+window.addEventListener('beforeinstallprompt', event => {
+    console.log('beforeinstallprompt fired');
+    event.preventDefault();
+    deferredPrompt = event;
+    document.getElementById("installnotification").style.display="block";
+    return false;
+  });
+
+document.getElementById("installPWA").addEventListener('click', event => {
+    event.preventDefault()
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice
+    .then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+      document.getElementById("installnotification").style.display="none";
+    });        
+});
+
+document.getElementById("cancelPWAInstall").addEventListener('click', event => {
+    event.preventDefault();
+    deferredPrompt = null;
+    document.getElementById("installnotification").style.display="none";
+})
+
