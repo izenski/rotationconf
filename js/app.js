7fc1b2892ae970f7aa1de98db41cf683c0a4a1f3
Steve Albers <stevealbers@hotmail.com>
2019-11-17 22:41:39 -0500

    Added metadata to notification

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw3.js')
+  navigator.serviceWorker.register('/sw0.js')
     .then(function () {
       console.log('[app] serviceWorker registered');
     })
@@ -9,56 +9,58 @@ if ('serviceWorker' in navigator) {
 }
 
 
-let deferredPrompt;
+// let deferredPrompt;
 
-window.addEventListener('beforeinstallprompt', event => {
-  console.log('beforeinstallprompt event');
-  event.preventDefault();
-  deferredPrompt = event;
-  document.getElementById("installnotification").style.display="block";
-  return false;
-});
+// window.addEventListener('beforeinstallprompt', event => {
+//   console.log('beforeinstallprompt event');
+//   event.preventDefault();
+//   deferredPrompt = event;
+//   document.getElementById("installnotification").style.display="block";
+//   return false;
+// });
 
-document.getElementById("installPWA").addEventListener('click', event => {
-  event.preventDefault();
-  deferredPrompt.prompt();
-  deferredPrompt.userChoice
-  .then((choiceResult) => {
-    if (choiceResult.outcome === 'accepted') {
-      console.log('User accepted the A2HS prompt');
-    } else {
-      console.log('User dismissed the A2HS prompt');
-    }
-    deferredPrompt = null;
-    document.getElementById("installnotification").style.display="none";
-  });        
-});
+// document.getElementById("installPWA").addEventListener('click', event => {
+//   event.preventDefault();
+//   deferredPrompt.prompt();
+//   deferredPrompt.userChoice
+//   .then((choiceResult) => {
+//     if (choiceResult.outcome === 'accepted') {
+//       console.log('User accepted the A2HS prompt');
+//     } else {
+//       console.log('User dismissed the A2HS prompt');
+//     }
+//     deferredPrompt = null;
+//     document.getElementById("installnotification").style.display="none";
+//   });        
+// });
 
-document.getElementById("cancelPWAInstall").addEventListener('click', event => {
-  event.preventDefault();
-  deferredPrompt = null;
-  document.getElementById("installnotification").style.display="none";
-})
+// document.getElementById("cancelPWAInstall").addEventListener('click', event => {
+//   event.preventDefault();
+//   deferredPrompt = null;
+//   document.getElementById("installnotification").style.display="none";
+// })
 
-
-let notificationEnabled = false;
 
 if ('Notification' in window) {
 document.getElementById("notifications")
   .addEventListener('click', event => {
-    if (notificationEnabled){
+    if (Notification.permission === "granted"){
       new Notification('Buy more stuff! ',
       {
         body: 'shop now for the best new widgets!',
-        tag: 'sale-offer'
+        icon: 'img/app-icon-96.png',
+        dir: 'ltr',
+        lang: 'en-US',
+        tag: 'sale-offer',
+        vibrate: [100,100,100],
+        badge: '/img/app-icon-96.png'
       })
     }
     else {        
       Notification.requestPermission(result => {
         //notification & push rights are granted together
-        console.log("Accept Notifications",result);
+        console.log("Request Notifications",result);
         if (result === 'granted'){
-          notificationEnabled = true;
           new Notification("Notifications are enabled!");
         }
       });
